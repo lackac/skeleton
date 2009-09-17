b6ec89db676b74cddb906c3c587d06ce9278bf82
Laszlo Bacsi <lackac@lackac.hu>
2009-09-17 15:05:45 +0200

    Added compass to dependencies and put v0.8.16 into vendor Modified skeleton to use compass with blueprint lib

--- app/routes/css.rb
+++ app/routes/css.rb
@@ -1,6 +1,12 @@
 class Main
   get "/css/:stylesheet.css" do
     content_type "text/css", :charset => "UTF-8"
-    sass :"css/#{params[:stylesheet]}"
+    css = sass(:"css/#{params[:stylesheet]}", Compass.sass_engine_options)
+    if Sinatra::Application.environment == :production
+      File.open(Compass.configuration.css_dir.join("#{params[:stylesheet]}.css"), "w") do |f|
+        f.write(css)
+      end
+    end
+    css
   end
 end
--- init.rb
+++ init.rb
@@ -12,10 +12,26 @@ require "monk/glue"
 require "ohm"
 require "haml"
 require "sass"
+require "compass"
 
 class Main < Monk::Glue
   set :app_file, __FILE__
+  set :public_path, root_path("public")
+  set :haml, { :format => :html5 }
   use Rack::Session::Cookie
+
+  configure do
+    Compass.configuration do |config|
+      config.project_path = root
+      config.sass_dir = File.join(views, "css")
+      config.css_dir = File.join(public_path, "css")
+      config.output_style = if environment == :production
+        :compressed
+      else
+        :expanded
+      end
+    end
+  end
 end
 
 # Connect to redis database.
